41db0515104f9d9d30c571c0d87f07a1d6863746
Wang, Congjian <congjian.wang@inl.gov>
2020-12-17 11:58:43 -0700

    add lock for external model

--- tests/framework/AnalyticModels/optimizing/myLocalSum.py
+++ tests/framework/AnalyticModels/optimizing/myLocalSum.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import threading
+localLock = threading.RLock()
 def evaluate(inputs):
   """
     Evaluates a weighted sum function.
